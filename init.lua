5f7222bc8283d03c62578d6f2eca25a21a0f0519
Robert <robinnolte7392@gmail.com>
2022-11-17 22:28:26 +0100

    set codelldb path

--- init.lua
+++ init.lua
@@ -301,9 +301,17 @@ local nvim_lsp = require 'lspconfig'
 
 -- RUST
 -- -------------------------------------
-local rt = require("rust-tools")
+local extension_path = '/codelldb/extension'
+local codelldb_path = extension_path .. 'adapter/codelldb'
+local liblldb_path = extension_path .. 'lldb/lib/liblldb.so'
 
-rt.setup({
+
+local opts = {
+    -- ... other configs
+    dap = {
+        adapter = require('rust-tools.dap').get_codelldb_adapter(
+            codelldb_path, liblldb_path)
+    },
     server = {
         on_attach = function(_, bufnr)
             -- Hover actions
@@ -311,8 +319,13 @@ rt.setup({
             -- Code action groups
             vim.keymap.set("n", "<Leader>a", rt.code_action_group.code_action_group, { buffer = bufnr })
         end,
-    },
-})
+    }
+}
+
+
+
+require("rust-tools").setup(opts)
+
 
 
 
