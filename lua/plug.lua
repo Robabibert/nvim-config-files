23a2c0bcb18fd12994ac9a5f0519fab274be4214
Robert Nolte <117081391+Robabibert@users.noreply.github.com>
2022-11-21 15:54:32 +0100

    Update plug.lua
    added surround.nvim

--- lua/plug.lua
+++ lua/plug.lua
@@ -89,6 +89,15 @@ return require('packer').startup(function()
           }
         end
       }
+    use({
+      "kylechui/nvim-surround",
+      tag = "*", -- Use for stability; omit to use `main` branch for the latest features
+      config = function()
+          require("nvim-surround").setup({
+              -- Configuration here, or leave empty to use defaults
+          })
+      end
+})
 
     if packer_bootstrap then
         require('packer').sync()
