b73e674cec9b2b7fee8af8041bf03821d6194406
Robert Nolte <117081391+Robabibert@users.noreply.github.com>
2022-11-22 10:42:05 +0100

    Update init.lua
    removed faulty comma

--- init.lua
+++ init.lua
@@ -315,7 +315,7 @@ rt.setup({
     server = {
         on_attach = function(_, bufnr)
             -- Code action groups
-            vim.keymap.set("n", "<Leader>a", rt.code_action_group.code_action_group, { buffer = bufnr }),
+            vim.keymap.set("n", "<Leader>a", rt.code_action_group.code_action_group, { buffer = bufnr })
         end,
     }
 })
